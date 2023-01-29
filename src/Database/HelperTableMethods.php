8f6cdda2893cd8d09a8f80d11ee249dc931deb37
ezegyfa2 <darvasvok@gmail.com>
2023-01-29 21:52:45 +0200

    Trigger

--- src/Database/HelperTableMethods.php
+++ src/Database/HelperTableMethods.php
@@ -10,9 +10,9 @@ class HelperTableMethods
 {
     public static function createHelperTable(array $baseTableNames, string $resultTableName) {
         \DB::statement(static::getCreateTableQuery($baseTableNames, $resultTableName));
-        dd('asd');
+        //dd(static::getCreateInsertTriggerQuery($baseTableNames, $baseTableNames[0], $resultTableName));
         foreach ($baseTableNames as $baseTableName) {
-            \DB::statement(static::getCreateTriggerQuery($baseTableName, $resultTableName));
+            \DB::statement(static::getCreateInsertTriggerQuery($baseTableNames, $baseTableName, $resultTableName));
         }
     }
 
@@ -22,8 +22,40 @@ class HelperTableMethods
             return $columnInfo->name . ' ' . $columnInfo->type;
         }, static::getColumnInfos($baseTableNames));
         $query .= StringMethods::concatenateStrings($columnParts, ', ');
-        $query .= ', PRIMARY KEY (`id`))';
+        $query .= ', PRIMARY KEY (id))';
         return $query;
+    }
+
+    protected static function getCreateInsertTriggerQuery(array $baseTableNames, string $triggerTableName, string $resultTableName) {
+        $triggerName = $resultTableName . '__' . $triggerTableName . '_insert';
+        $columnNames = StringMethods::concatenateStrings(static::getColumnNames($baseTableNames), ', ');
+        $columnValues = StringMethods::concatenateStrings(static::getColumnValues($baseTableNames, $triggerTableName), ', ');
+        return 'CREATE TRIGGER ' . $triggerName
+            . ' AFTER INSERT'
+            . ' ON ' . $triggerTableName . ' FOR EACH ROW'
+            . ' BEGIN'
+            . ' INSERT INTO test(' . $columnNames . ') VALUES(' . $columnValues . ');'
+            . ' END;';
+    }
+
+    protected static function getColumnValues(array $baseTableNames, string $insertTableName) {
+        $columnNames = static::getColumnNames($baseTableNames);
+        $insertColumnNames = static::getColumnNames([ $insertTableName ]);
+        return array_map(function($columnName) use($insertTableName, $insertColumnNames) {
+            if (in_array($columnName, $insertColumnNames)) {
+                $insertedColumnName = str_replace($insertTableName . '__', '', $columnName);
+                return 'NEW.' . $insertedColumnName;
+            }
+            else {
+                return 'null';
+            }
+        }, $columnNames);
+    }
+
+    protected static function getColumnNames(array $baseTableNames) {
+        return array_map(function($columnInfo) {
+            return $columnInfo->name;
+        }, static::getColumnInfos($baseTableNames));
     }
 
     protected static function getColumnInfos(array $baseTableNames) {
@@ -44,15 +76,15 @@ class HelperTableMethods
         return $helperColumnInfos;
     }
 
-    protected static function getCreateTriggerQuery(string $baseTableName, string $resultTableName) {
-        $triggerName = $resultTableName . '__' . $baseTableName . '_insert';
-        return 'DELIMITER $$'
-            . ' CREATE TRIGGER ' . $triggerName
-            . ' AFTER INSERT'
-            . ' ON ' . $baseTableName . ' FOR EACH ROW'
-            . ' BEGIN'
-            . ' INSERT INTO test(product_details__stock) VALUES(3);'
-            . ' END$$'
-            . ' DELIMITER ;';
+    public static function select(string $query) {
+        $rows = \DB::select($query);
+        return array_map(function($row) {
+            $convertedRow = new \stdClass();
+            foreach ($row as $key => $value) {
+                $convertedKey = strtolower($key);
+                $convertedRow->$convertedKey = $value;
+            }
+            return $convertedRow;
+        }, $rows);
     }
 }
